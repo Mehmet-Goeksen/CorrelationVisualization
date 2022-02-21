6ad96246aec163573ea760b5f97e05c856fdf6d6
Mehmet-Goeksen <mehmet.goeksen@gmail.com>
2022-02-21 23:30:18 +0100

    removed deleted file import from views.py

--- correlationAnalysis/correlationVisualization/views.py
+++ correlationAnalysis/correlationVisualization/views.py
@@ -1,11 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
-from . import SingleDomain_DataType_DataUsage_Relative as heatmap
 from . import SingleDomain_DataType_DataUsage_Relative_hover as hover
 
-    #def index(self, request):
-    #    return HttpResponse("Hello World")
     
 def get(request, **kwargs):
     return render(request, 'index.html', context=None)
@@ -14,14 +11,12 @@ def post(request):
     domain = request.POST['domain']
     amount = int(request.POST['corr'])
     corr = hover.plotHeatmap(domain, amount)
-    #corr = heatmap.plotHeatmap(domain, amount)
     corr = corr.to_dict()
     context_dict = {}
     context_dict['res'] = corr
     context_dict['domain'] = hover.getDomainName()
     context_dict['sampleSize'] = hover.getSampleSize()
     print(hover.getDomainName())
-    #heatmap.get_top_abs_correlations(corr)
     return render(request, 'resultHover.html', context=context_dict)
 
 def legend(request):
